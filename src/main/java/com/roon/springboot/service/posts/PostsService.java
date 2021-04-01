9f7e04e9af71f1e9ac63e2527f093e7ff78d94cc
auddl0756 <cjddl789123@naver.com>
2021-04-01 14:56:29 +0900

    ch4(화면 처리)까지

--- src/main/java/com/roon/springboot/service/posts/PostsService.java
+++ src/main/java/com/roon/springboot/service/posts/PostsService.java
@@ -2,13 +2,17 @@ package com.roon.springboot.service.posts;
 
 import com.roon.springboot.domain.posts.Posts;
 import com.roon.springboot.domain.posts.PostsRepository;
+import com.roon.springboot.web.dto.PostsListResponseDto;
 import com.roon.springboot.web.dto.PostsResponseDto;
 import com.roon.springboot.web.dto.PostsSaveRequestDto;
 import com.roon.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,4 +40,22 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("no board. id="+id));
+
+        postsRepository.delete(posts);
+    }
+
+
+
 }
