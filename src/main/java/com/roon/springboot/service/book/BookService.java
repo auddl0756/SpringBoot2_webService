9b1357a0e80a991acae7c3e4229ffceb719852ed
auddl0756 <cjddl789123@naver.com>
2022-01-17 22:31:04 +0900

    propagation = REQUIRED / 기존(첫) 트랜잭션이 주도함.

--- src/main/java/com/roon/springboot/service/book/BookService.java
+++ src/main/java/com/roon/springboot/service/book/BookService.java
@@ -5,7 +5,10 @@ import com.roon.springboot.web.exception.NoStockException;
 import com.roon.springboot.web.exception.NotEnoughMoneyException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -14,23 +17,25 @@ public class BookService {
     private final BookStockRepository bookStockRepository;
     private final AccountRepository accountRepository;
 
-    @Transactional
-    public void purchase(int count){
+
+//    @Transactional
+    @Transactional(propagation = Propagation.REQUIRED)  // 기존 트랙잭션이 있다면, 기존 트랜잭션이 주도함
+    public void purchase(String username, String bookNumber){
         // 1. 책 가격 조회
         // 2. 통장 잔고 조회
         // 3. 살 수 있으면 책 사기 , 책 재고 줄이기, 통장 잔고 줄이기
 
-        Book book = bookRepository.findById("booknumber")
+        Book book = bookRepository.findById(bookNumber)
                 .orElseThrow(()-> new IllegalArgumentException("해당 책 번호 없음"));
 
-        int price = book.getPrice() * count;
+        int price = book.getPrice();
 
-        Account account = accountRepository.findById("sample account id")
+        Account account = accountRepository.findById(username)
                 .orElseThrow(()-> new IllegalArgumentException("해당 계좌 아이디 없음"));
 
         int balance = account.getBalance();
 
-        BookStock bookStock = bookStockRepository.findById("booknumber")
+        BookStock bookStock = bookStockRepository.findById(bookNumber)
                 .orElseThrow(() -> new IllegalArgumentException("해당 책 번호의 재고 정보 없음"));
 
 
@@ -44,6 +49,13 @@ public class BookService {
         }
 
         account.pay(price);
-        bookStock.sell(count);
+        bookStock.sell(1);
+    }
+
+    @Transactional
+    public void checkout(List<String> bookNumbers, String username){
+        for(String bookNumber : bookNumbers){
+            purchase(bookNumber,username);
+        }
     }
 }
